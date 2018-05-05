41c90393cbe8c2eb59e14a51c4bd26fda3d6fe43
Simon Wunderlin <swunderlin@gmail.com>
2018-05-05 09:50:12 +0200

    added simple FXML app

--- hellofx/src/hellofx/Main.java
+++ hellofx/src/hellofx/Main.java
@@ -1,26 +1,54 @@
 package hellofx;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
-
 public class Main extends Application {
+	private BorderPane rootLayout;
+	private Stage primaryStage;
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
 	@Override
 	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("My App");
+		
+		initRootLayout();
+		showContent();
+	}
+	
+	public void initRootLayout() {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main.class.getResource("view/RootLayout.fxml"));
+			rootLayout = (BorderPane) loader.load();
+			
+			Scene scene = new Scene(rootLayout);
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
-	public static void main(String[] args) {
-		launch(args);
-	}
+	public void showContent() {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main.class.getResource("view/View.fxml"));
+			AnchorPane view = (AnchorPane) loader.load();
+			
+			rootLayout.setCenter(view);
+						
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}	
 }
